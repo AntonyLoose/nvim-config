171a51e7c267f300c2c9b64cb0cc2ec0ccd338c8
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-04-21 13:43:06 +1000

    feat: ts snippets

---
+++ lua/ant/snippets/ts.lua
@@ -0,0 +1,17 @@
+require("luasnip.session.snippet_collection").clear_snippets("ts")
+
+local ls = require("luasnip")
+
+local s = ls.snippet
+local i = ls.insert_node
+
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("typescript", {
+    s("for", fmt("for (let {}=0; {} < {}; {}++){{\n\t{}\n}}", { i(1), i(2), i(3), i(4), i(5) })),
+    s("await", fmt("await new Promise((res, rej) => setTimeout(res, {}));", { i(1) })),
+    s("leet",
+        fmt(
+            "const assert = require('assert');\n\nfunction {}({}) {{\n\n}}\n\n// --- Testing --- //\n\n{}",
+            { i(1), i(2), i(3) }))
+})
