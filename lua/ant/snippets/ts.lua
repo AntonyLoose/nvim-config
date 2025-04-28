26cc502a1d59c308dbb8295b59af59d579d25492
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-04-28 15:26:49 +1000

    feat: ts class snippet

--- lua/ant/snippets/ts.lua
+++ lua/ant/snippets/ts.lua
@@ -13,6 +13,7 @@ ls.add_snippets("typescript", {
     s("for", fmt("for (let {}=0; {} < {}; {}++){{\n\t{}\n}}", { i(1), i(2), i(3), i(4), i(5) })),
     s("forr", fmt("for (const {} of {}){{\n\t{}\n}}", { i(1), i(2), i(3) })),
     s("await", fmt("await new Promise((res, rej) => setTimeout(res, {}));", { i(1) })),
+    s("class", fmt("export class {} {{\n\tpublic constructor({}){{\n\t{}\n\t}}\n}}", { i(1), i(2), i(3) })),
     s("leet",
         fmt(
             "const assert = require('assert');\n\n/**\n* {}\n*/\nfunction {}({}) {{\n\n}}\n\n// --- Testing --- //\n\n{}",
