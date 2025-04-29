71a42aeb2fab2de47925aa3d67fdee30a8935cdf
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-04-29 16:44:36 +1000

    refactor: condense js and ts snippets to single file

--- lua/ant/snippets/js.lua
+++ lua/ant/snippets/js.lua
@@ -1,4 +1,5 @@
 require("luasnip.session.snippet_collection").clear_snippets("js")
+require("luasnip.session.snippet_collection").clear_snippets("ts")
 
 local ls = require("luasnip")
 
@@ -7,16 +8,21 @@ local i = ls.insert_node
 
 local fmt = require("luasnip.extras.fmt").fmt
 
-ls.add_snippets("javascript", {
+snippets = {
     s("/*", fmt("/*\n* {}\n*/", { i(1) })),
     s("/**", fmt("/**\n* {}\n*/", { i(1) })),
     s("//-", fmt("// ---- {} ---- //", { i(1) })),
     s("?", fmt("{} ? {} : {}", { i(1), i(2), i(3) })),
+    s("if", fmt("if ({}) {{\n\t{}\n}}", { i(1), i(2) })),
     s("for", fmt("for (let {}=0; {} < {}; {}++){{\n\t{}\n}}", { i(1), i(2), i(3), i(4), i(5) })),
     s("forr", fmt("for (const {} of {}){{\n\t{}\n}}", { i(1), i(2), i(3) })),
     s("await", fmt("await new Promise((res, rej) => setTimeout(res, {}));", { i(1) })),
+    s("class", fmt("export class {} {{\n\tpublic constructor({}){{\n\t{}\n\t}}\n}}", { i(1), i(2), i(3) })),
     s("leet",
         fmt(
             "const assert = require('assert');\n\n/**\n* {}\n*/\nfunction {}({}) {{\n\n}}\n\n// --- Testing --- //\n\n{}",
             { i(1), i(2), i(3), i(4) }))
-})
+}
+
+ls.add_snippets("javascript", snippets)
+ls.add_snippets("typescript", snippets)
