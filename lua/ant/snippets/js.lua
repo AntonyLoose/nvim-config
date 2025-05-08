84b83600ba3b3a886b48d1d5ae2470e10a3ff708
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-05-08 15:52:12 +1000

    feat: add some sexy snippets

--- lua/ant/snippets/js.lua
+++ lua/ant/snippets/js.lua
@@ -5,6 +5,7 @@ local ls = require("luasnip")
 
 local s = ls.snippet
 local i = ls.insert_node
+local rep = require("luasnip.extras").rep
 
 local fmt = require("luasnip.extras.fmt").fmt
 
@@ -15,10 +16,12 @@ snippets = {
     s("log", fmt('console.log("----- {} -----");', { i(1) })),
     s("?", fmt("{} ? {} : {}", { i(1), i(2), i(3) })),
     s("if", fmt("if ({}) {{\n\t{}\n}}", { i(1), i(2) })),
-    s("for", fmt("for (let {}=0; {} < {}; {}++){{\n\t{}\n}}", { i(1), i(2), i(3), i(4), i(5) })),
+    s("for", fmt("for (let {}=0; {} < {}; {}++){{\n\t{}\n}}", { i(1), rep(1), i(2), rep(1), i(3) })),
     s("forr", fmt("for (const {} of {}){{\n\t{}\n}}", { i(1), i(2), i(3) })),
     s("await", fmt("await new Promise((res, rej) => setTimeout(res, {}));", { i(1) })),
     s("class", fmt("export class {} {{\n\tpublic constructor({}){{\n\t{}\n\t}}\n}}", { i(1), i(2), i(3) })),
+    s("this", fmt("this.{} = {}", { i(1), rep(1) })),
+    s("this_", fmt("this._{} = {}", { i(1), rep(1) })),
     s("err", fmt("throw new Error(`{}`);", { i(1) })),
     s("errm", fmt('throw new Error("Method not implemented yet.");', {})),
     s("leet",
