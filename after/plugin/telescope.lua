275972aa40b30f7faee2b19152561b6925ea11f0
Antony Loose <antony35007@gmail.com>
2025-06-12 04:18:28 +0000

    feat: telescope to ignore node_modules

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,14 @@
-local builtin = require('telescope.builtin')
+local ts = require('telescope')
+ts.setup({
+    defaults = {
+        file_ignore_patterns = { "node_modules" }
+    }
+})
+
+local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
 vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope git files' })
-
